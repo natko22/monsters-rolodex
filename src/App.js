9f59b2517ba0f620b016b25baac5722477a2b37e
Natassa <112552395+natko22@users.noreply.github.com>
2023-05-29 09:48:15 +0200

    filter and include method

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -26,10 +27,28 @@ class App extends Component {
         )
       );
   }
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={onSearchChange}
+        />
+
+        {filteredMonsters.map((monster) => {
           return (
             <div>
               <h1 key={monster.id}>{monster.name}</h1>
